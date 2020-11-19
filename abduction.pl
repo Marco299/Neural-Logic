9b889fd963f11db0745c111e721dc3eac029170b
Marco <iannotta.ma@gmail.com>
2020-11-19 12:15:17 +0100

    Modify symbolic module to compute sum of n digits

--- abduction.pl
+++ abduction.pl
@@ -1,5 +1,5 @@
 :- module(abduction,[
-  generate_abductions/2
+  generate_abductions/3
 	]).
 
 :- ensure_loaded(library(lists)).
@@ -7,41 +7,58 @@
 dig(X) :-
   member(X, [0,1,2,3,4,5,6,7,8,9]).
 
-add(X,Y,W,Z) :-
-  dig(X),
-  dig(Y),
-  dig(W),
-  Z is X+Y+W.
+add(Addends,AddendsNum,Sum) :-
+    add(Addends, 0, _, AddendsNum, 0, Sum).
 
-generate_abductions(SumList, AbductionList) :-
-    generate_abductions(SumList, 0, _, [], AbductionList).
+add([], AddendsNum, _, AddendsNum, Sum1, Sum) :-
+    !,
+    Sum is Sum1.
 
-generate_abductions([], _, _, List, List).
-generate_abductions([H|T], IndexIn, IndexOut, ListIn, ListOut) :-
-    findall([X,Y,W], add(X,Y,W,H), Abductions),
-    add_abduction(Abductions, IndexIn, ListIn, ListPart),
+add([H|T], AddendsNumIn, AddendsNumOut, AddendsNum, SumIn, SumOut) :-
+    dig(H),
+    AddendsNumPart is AddendsNumIn + 1,
+    SumPart is SumIn + H,
+    add(T, AddendsNumPart, AddendsNumOut, AddendsNum, SumPart, SumOut).
+
+generate_abductions(SumList, AddendsNum, AbductionList) :-
+    generate_abductions(SumList, AddendsNum, 0, _, [], AbductionList).
+
+generate_abductions([], _, _, _, List, List).
+generate_abductions([H|T], AddendsNum, IndexIn, IndexOut, ListIn, ListOut) :-
+    findall(L, add(L, AddendsNum, H), Abductions),
+    add_abduction(Abductions, AddendsNum, IndexIn, ListIn, ListPart),
     IndexPart is IndexIn+1,
-    generate_abductions(T, IndexPart, IndexOut, ListPart, ListOut).
+    generate_abductions(T, AddendsNum, IndexPart, IndexOut, ListPart, ListOut).
 
-add_abduction(Abductions, SumIndex, [], [[AddendsIndexes, Abductions]]) :-
-    generate_indexes(SumIndex, AddendsIndexes),
+add_abduction(Abductions, AddendsNum, SumIndex, [], [[AddendsIndexes, Abductions]]) :-
+    generate_indexes(SumIndex, AddendsNum, AddendsIndexes),
     !.
 
-add_abduction(Abductions, SumIndex, [[Indexes,Abductions1]|T], UpdatedList) :-
+add_abduction(Abductions, AddendsNum, SumIndex, [[Indexes,Abductions1]|T], UpdatedList) :-
     length(Abductions, AbductionsLen),
     length(Abductions1, Abductions1Len),
     Abductions1Len >= AbductionsLen,
     !,
-    generate_indexes(SumIndex, AddendsIndexes),
+    generate_indexes(SumIndex, AddendsNum, AddendsIndexes),
     PartList = [[AddendsIndexes, Abductions], [Indexes,Abductions1]],
     append(PartList, T, UpdatedList).
 
-add_abduction(Abductions, SumIndex, [[Indexes,Abductions1]|T], UpdatedList) :-
-    add_abduction(Abductions, SumIndex, T, PartList),
+add_abduction(Abductions, AddendsNum, SumIndex, [[Indexes,Abductions1]|T], UpdatedList) :-
+    add_abduction(Abductions, AddendsNum, SumIndex, T, PartList),
     append([[Indexes,Abductions1]], PartList, UpdatedList).
 
-generate_indexes(SumIndex, AddendsIndexes) :-
-    FirstAddendIndex is SumIndex*3,
-    SecondAddendIndex is FirstAddendIndex+1,
-    ThirdAddendIndex is SecondAddendIndex+1,
-    AddendsIndexes = [FirstAddendIndex, SecondAddendIndex, ThirdAddendIndex].
+generate_indexes(SumIndex, AddendsNum, AddendsIndexes) :-
+    FirstIndex is SumIndex*AddendsNum,
+    generate_indexes(FirstIndex, AddendsNum, [FirstIndex], AddendsIndexesRev),
+    reverse(AddendsIndexesRev, AddendsIndexes).
+
+generate_indexes(_, AddendsNum, AddendsIndexes, AddendsIndexes) :-
+    length(AddendsIndexes, AddendsIndexesLen),
+    AddendsNum == AddendsIndexesLen,
+    !.
+
+generate_indexes(Index, AddendsNum, AddendsIndexesIn, AddendsIndexesOut) :-
+    NewIndex is Index + 1,
+    append([NewIndex], AddendsIndexesIn, AddendsIndexesPart),
+    generate_indexes(NewIndex, AddendsNum, AddendsIndexesPart, AddendsIndexesOut).
+
